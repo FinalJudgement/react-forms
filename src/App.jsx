25b4794830a9a00d1e3c176629b520b76e694975
Carlos Silva <silvacj90@gmail.com>
2023-11-17 16:47:47 -0500

    complete form assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [token, setToken] = useState(null);
 
   return (
     <>
-      <div></div>
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token} />
     </>
   );
 }
